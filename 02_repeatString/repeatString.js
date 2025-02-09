2df8855902870ceea433571d9a1cb69c5074e4f5
Mesi14 <Mesi18@proton.me>
2025-02-09 09:33:31 +0200

    :sparkles: solve exercises 1,2,3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function(mystr, num) {
+  if(num < 0) return 'ERROR';
+  let res = '';
+  for(let i=0; i<num; i++) res+=mystr;
+  return res;
 };
 
 // Do not edit below this line
