179401dd36760ea05e6236e5a42ae88a894fc743
Mesi14 <Mesi18@proton.me>
2025-02-19 11:10:39 +0200

    :sparkles: solve palindromes challenge

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function(s) {
+    let cleanedS = s.replace(/[^0-9A-Z]+/gi,"");
+    let reverseS = cleanedS.toLowerCase().split("").reverse().join("");
+    return reverseS == cleanedS.toLowerCase() ? true : false;
 };
 
 // Do not edit below this line
