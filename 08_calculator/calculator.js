058e1cf1c43f188deb96212ff651294a2273b3a4
Mesi14 <Mesi18@proton.me>
2025-02-19 10:14:48 +0200

    :sparkles: solve calculator challenge

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,27 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((accum, val) =>  accum += val, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((accum, val) =>  accum *= val, 1);
 };
 
-const power = function() {
-	
+const power = function(a, pow) {
+	return a ** pow;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if(num < 0) return "Can't solve for negative numbers";
+	if(num === 0) return 1;
+	return num * factorial(num-1);
 };
 
 // Do not edit below this line
